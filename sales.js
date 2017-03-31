65a1d8ad0d9e865f8e3137c252353fce34c4a30c
Unknown <mshimada2@mail.csuchico.edu>
2017-03-30 21:29:32 -0700

    made it so you can enter numbers and calculate the tax along with cacuating the whoe receipt

--- sales.js
+++ sales.js
@@ -2,8 +2,8 @@ var runningTotal = 0.0;
 
 function addItem()
 {
-  var newItem;
-  newItem = document.getElementById("price").value;
+  var newItem = document.getElementById("price").value;
+
   //IF newItem is not a number
   // THEN show an alert: "Enter price as a number"
 if (isNaN(newItem)) {
@@ -12,24 +12,32 @@ if (isNaN(newItem)) {
 
 }
 
-else{
+else{//OTHERWISE,
+newItem = Number(newItem);  // update newItem to its value cast as a number
+runningTotal = runningTotal + newItem; // update runningTotal to be its value plus newItem
+var dollars; // create a variable called dollars
+dollars = asCurrency(runningTotal); // call asCurrency() by with the value of runningTotal and assign the return value to dollars
+document.getElementById("subtotal").innerHTML = dollars;// update the innerHTML of the span with the id "subtotal" to be dollars
+document.getElementById("price").innerHTML = "";  // update the value of the input with the id "price" to be an empty string
+setCookie("preTax", runningTotal, 1);  // update a cookie called "preTax" with the value of runningTotal
 
-alert("Accepted");
 
 }
 }
 
-  //OTHERWISE,
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
-  // update a cookie called "preTax" with the value of runningTotal
+
+
+
+
+
+
+
 
 
 //takes a number and gives a string with the number displayed as USD currency
+
+
+
 function asCurrency(val)
 {
   return "$" + val.toFixed(2);
@@ -56,4 +64,23 @@ function getCookie(cname) {
         }
     }
     return "";
+}
+
+
+
+function calculateReceipt() {
+
+  var receiptSubtotal = getCookie("preTax");
+  receiptSubtotal = Number(receiptSubtotal);
+
+  var receiptTax = receiptSubtotal * 0.075;
+  var receiptTotal = receiptSubtotal + receiptTax;
+
+    document.getElementById("sub").innerHTML = asCurrency(receiptSubtotal);
+     document.getElementById("tax").innerHTML = asCurrency(receiptTax);
+     document.getElementById("tot").innerHTML = asCurrency(receiptTotal);
+
+
+
+
 }
